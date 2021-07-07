591a820f0aed4dcb9848090ef0d5feb57262c793
Farah Warfa <fwarfa@Farahs-MBP.roc.mn.charter.com>
2021-07-07 11:21:48 -0500

    completed assignment

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -21,7 +21,7 @@ addToCollection('I see you', 'Jon', '2013');
 addToCollection('The Way You Look At Me', 'Fadumo', '2019');
 addToCollection('Tears', 'Argason', '2020');
 addToCollection('Changing', 'Britney', '2015');
-addToCollection('Choosing', 'Bob', '1990');
+addToCollection('Choosing', 'Tim', '1990');
 
 console.log(collection);
 
@@ -34,3 +34,21 @@ function showCollection(array) {
     }
 }
 showCollection(collection);
+
+// Creating function that will find inputed artist from the collection and will push it to the results array
+function findByArtist (artist) {
+    let results = [];
+    for (let item of collection) {
+        if (item.artist === artist) {
+            results.push(item);
+            console.log('artist found');
+        }
+        else {
+            console.log('artist not found');
+        }
+    }
+    return console.log(results);
+}
+
+// Testing findByArtist funciton 
+findByArtist('Tim');
